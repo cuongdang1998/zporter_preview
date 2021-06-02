1130cd1b92e95c160e02a6e855f71cf37a4e9758
cuongdq <cuong.dang@vmodev.com>
2021-06-02 10:38:16 +0700

    add gradient

--- lib/presentation/screen_dashboard/ui/widget/widget_custom_line_chart.dart
+++ lib/presentation/screen_dashboard/ui/widget/widget_custom_line_chart.dart
@@ -26,82 +26,71 @@ class CustomLineChart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 160,
-      child: Stack(
-        children: [
-          // Container(
-          //   decoration: BoxDecoration(
-          //     gradient: RadialGradient(
-          //       colors: [
-          //         AppColors.black3Color,
-          //         AppColors.blueColor.withOpacity(.3),
-          //         AppColors.blueColor.withOpacity(.3),
-          //         AppColors.blueColor.withOpacity(.3),
-          //         AppColors.blueColor.withOpacity(.3),
-          //         AppColors.black3Color,
-          //       ],
-          //     ),
-          //   ),
-          // ),
-          Visibility(
-            visible: showHorizontalLine,
-            child: Positioned(
-              top: comparePoint,
-              right: 0,
-              left: 0,
-              child: Container(
-                height: 1,
-                width: double.infinity,
-                color: AppColors.greyColor,
+    return CustomPaint(
+      painter: GradientPainter(),
+      child: Container(
+        width: double.infinity,
+        height: 160,
+        child: Stack(
+          children: [
+            Visibility(
+              visible: showHorizontalLine,
+              child: Positioned(
+                top: comparePoint,
+                right: 0,
+                left: 0,
+                child: Container(
+                  height: 1,
+                  width: double.infinity,
+                  color: AppColors.greyColor,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: LineChart(
-              LineChartData(
-                borderData: FlBorderData(
-                  show: false,
-                ),
-                // handle touch on the point of line chart
-                lineTouchData: LineTouchData(
-                  enabled: false,
-                ),
-                // handle visible data on x,y axis
-                titlesData: FlTitlesData(
-                  show: false,
-                ),
-                // handle grid view background
-                gridData: FlGridData(
-                  show: false,
-                ),
-                // list of line bar
-                lineBarsData: [
-                  LineChartBarData(
-                    spots: youData,
-                    isCurved: false,
-                    barWidth: 3,
-                    colors: [youColor],
-                    dotData: getDotStyle(
-                      color: youColor,
-                    ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: LineChart(
+                LineChartData(
+                  borderData: FlBorderData(
+                    show: false,
                   ),
-                  LineChartBarData(
-                    spots: averageData,
-                    isCurved: false,
-                    barWidth: 3,
-                    colors: [averageColor],
-                    dotData: getDotStyle(
-                      color: averageColor,
-                    ),
+                  // handle touch on the point of line chart
+                  lineTouchData: LineTouchData(
+                    enabled: false,
                   ),
-                ],
+                  // handle visible data on x,y axis
+                  titlesData: FlTitlesData(
+                    show: false,
+                  ),
+                  // handle grid view background
+                  gridData: FlGridData(
+                    show: false,
+                  ),
+                  // list of line bar
+                  lineBarsData: [
+                    LineChartBarData(
+                      spots: youData,
+                      isCurved: false,
+                      barWidth: 3,
+                      colors: [youColor],
+                      dotData: getDotStyle(
+                        color: youColor,
+                      ),
+                    ),
+                    LineChartBarData(
+                      spots: averageData,
+                      isCurved: false,
+                      barWidth: 3,
+                      colors: [averageColor],
+                      dotData: getDotStyle(
+                        color: averageColor,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -115,5 +104,33 @@ class CustomLineChart extends StatelessWidget {
         strokeColor: color,
       );
     });
+  }
+}
+
+class GradientPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Path path = Path()
+      ..addRect(
+        Rect.fromPoints(
+          Offset.zero,
+          Offset(size.width - 20, size.height),
+        ),
+      )
+      ..fillType = PathFillType.nonZero;
+    Paint paint = Paint()
+      ..color = AppColors.blueColor.withOpacity(.18)
+      ..maskFilter =
+          MaskFilter.blur(BlurStyle.normal, convertRadiusToSigma(size.width));
+    canvas.drawPath(path, paint);
+  }
+
+  static double convertRadiusToSigma(double radius) {
+    return radius * 0.57735 + 0.5;
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
